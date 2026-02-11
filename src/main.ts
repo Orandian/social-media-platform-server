1817c640aec5340bb704793354ecb9f92087835b
Yan Naing Htwe <yannainghtwe.777.yy@example.com>
2026-02-11 23:58:01 +0900

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Apply global pipes
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +13,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, "0.0.0.0");
+
+  console.log(`🚀 Server running on port ${port}`);
 }
 bootstrap();
